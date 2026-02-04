03e4bc4746e364929d297c669fab4972a4e1cc9a
saewitz <jonathansaewitz@gmail.com>
2026-02-04 13:08:37 -0500

    use AI to review procedure documentation

---
+++ packages/zambdas/src/ehr/ai-suggestion-notes/index.ts
@@ -0,0 +1,59 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+import { fixAndParseJsonObjectFromString, getSecret, SecretsKeys } from 'utils';
+import { topLevelCatch, wrapHandler, ZambdaInput } from '../../shared';
+import { invokeChatbot } from '../../shared/ai';
+import { validateRequestParameters } from './validateRequestParameters';
+
+export const index = wrapHandler('ai-suggestion-notes', async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
+  try {
+    console.group('validateRequestParameters');
+    const validatedParameters = validateRequestParameters(input);
+    const { type, details, secrets } = validatedParameters;
+    console.groupEnd();
+    console.debug('validateRequestParameters success');
+
+    let prompt = undefined;
+
+    if (type === 'procedure') {
+      const { procedureDetails } = details;
+      prompt = `If the procedure material type and quantity are missing, return this message:
+      
+      "Please specify the closure type and quantity" 
+
+      Only return this message if the text describes a wound or incision but does not include any or these: material type, length and a numerical count.
+      
+      If these details are included return this message: "Procedure details are included".
+
+      Return a JSON object with a single field "suggestions" that has a list of strings.
+      
+      ${procedureDetails}`;
+    }
+
+    if (!prompt) {
+      throw new Error('prompt is not defined');
+    }
+
+    const aiResponseString = (await invokeChatbot([{ role: 'user', content: prompt }], secrets)).content.toString();
+    console.log(aiResponseString);
+
+    let aiResponseObject;
+    try {
+      aiResponseObject = JSON.parse(aiResponseString);
+    } catch (parseError) {
+      console.warn('Failed to parse AI recommendations response, attempting to fix JSON format:', parseError);
+      aiResponseObject = fixAndParseJsonObjectFromString(aiResponseString);
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(aiResponseObject),
+    };
+  } catch (error: any) {
+    const ENVIRONMENT = getSecret(SecretsKeys.ENVIRONMENT, input.secrets);
+    await topLevelCatch('ai-suggestion-notes', error, ENVIRONMENT);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: `Error getting ai suggestions: ${error}` }),
+    };
+  }
+});
---
+++ packages/zambdas/src/ehr/ai-suggestion-notes/validateRequestParameters.ts
@@ -0,0 +1,29 @@
+import { AISuggestionNotesInput, MISSING_REQUIRED_PARAMETERS } from 'utils';
+import { ZambdaInput } from '../../shared';
+
+export function validateRequestParameters(input: ZambdaInput): AISuggestionNotesInput & Pick<ZambdaInput, 'secrets'> {
+  if (!input.body) {
+    throw new Error('No request body provided');
+  }
+
+  // no complication
+  const { type, details } = JSON.parse(input.body);
+
+  if (!type.includes('procedure')) {
+    throw new Error('Invalid type');
+  }
+
+  if (!details) {
+    throw MISSING_REQUIRED_PARAMETERS(['details']);
+  }
+
+  if (type === 'procedure' && !details.procedureDetails) {
+    throw new Error('If type is procedure, procedureDetails is required');
+  }
+
+  return {
+    type,
+    details,
+    secrets: input.secrets,
+  };
+}
